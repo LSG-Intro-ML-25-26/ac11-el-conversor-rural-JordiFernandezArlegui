5ca91990eaccf410719ad3f6b681d0907f6d0531
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-12-18 19:01:38 +0100

    audio npc

--- main.py
+++ main.py
@@ -438,7 +438,7 @@ def on_on_overlap(sprite2, otherSprite):
         elif game.ask("Vols " + NPC_inventari[i1] + ("? Tinc " + NPC_inventari[i2])):
             input2 = game.ask_for_number("Digue'm, quants vols?", 2)
             indiceListas = producte2.index_of(NPC_inventari[i1])
-            cost = Math.round(input2 * (preu[indiceListas]*100 / cantitat[indiceListas]*100) / 10000)
+            cost = Math.round(input2 * (preu[indiceListas] * 100 / cantitat[indiceListas] * 100) / 10000)
             if input2 > parse_float(NPC_inventari[i2]) or input2 % 1 != 0 or input2 <= 0:
                 if input2 % 1 != 0:
                     game.splash("No ho dividiré a troços!")
@@ -458,17 +458,17 @@ def on_on_overlap(sprite2, otherSprite):
                 game.splash("Torna quan puguis pagar el que vols!")
         else:
             game.show_long_text("Oh... Una pena...\\nEns veiem!", DialogLayout.BOTTOM)
-    if 0 == PlayerInventoryLlenya:
-        game.show_long_text("FELICITATS!", DialogLayout.FULL)
-        game.show_long_text("HAS GUANYAT!", DialogLayout.FULL)
-        game.show_long_text("Ets un fraca sense un sol duro!", DialogLayout.FULL)
-        game.show_long_text("*FI DEL JOC*", DialogLayout.FULL)
-        game.reset()
-    else:
-        pause(100)
-        music.play(music.melody_playable(music.jump_down),
-            music.PlaybackMode.IN_BACKGROUND)
-        music.set_volume(120)
+        if 0 == PlayerInventoryLlenya:
+            game.show_long_text("FELICITATS!", DialogLayout.FULL)
+            game.show_long_text("HAS GUANYAT!", DialogLayout.FULL)
+            game.show_long_text("Ets un fraca sense un sol duro!", DialogLayout.FULL)
+            game.show_long_text("*FI DEL JOC*", DialogLayout.FULL)
+            game.reset()
+        else:
+            pause(100)
+            music.play(music.melody_playable(music.jump_down),
+                music.PlaybackMode.IN_BACKGROUND)
+            music.set_volume(120)
 sprites.on_overlap(SpriteKind.player, SpriteKind.NPC, on_on_overlap)
 
 def on_up_pressed():
@@ -776,7 +776,7 @@ music.play(music.create_song(hex("""
         0078000408020600001c00010a006400f401640000040000000000000000000000000005000004360000000400011d08000c00011910001400011e14001800011918001c00011b1c002000011d20002400012028002c00011e30003400011d01001c000f05001202c102c20100040500280000006400280003140006020004180000000400012510001400012420002400012730003400012503001c0001dc00690000045e0100040000000000000000000005640001040003060038004000012907001c00020a006400f401640000040000000000000000000000000000000003360000000400012008000c00011d10001400011b14001800011d18001c00011e1c002000012020002400012428002c00012230003400012008001c000e050046006603320000040a002d0000006400140001320002010002180000001000012910002000012720003000012a30004000012909010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8003000000001000100080009000100100011000100180019000100200021000100280029000100300031000100380039000100
         """)),
     music.PlaybackMode.LOOPING_IN_BACKGROUND)
-music.set_volume(10)
+music.set_volume(32)
 NPCs = [sprites.create(img("""
             . . . . . . f f f f . . . . . .
             . . . . f f f 2 2 f f f . . . .
